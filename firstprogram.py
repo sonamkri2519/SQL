71674ccd3926770c7bbe42059ced7ef0900473c5
sonamkri2519 <miss082sonam@gmail.com>
2025-10-07 12:41:58 +0530

    Add files via upload

---
+++ firstprogram.py
@@ -0,0 +1,32 @@
+# name=input("enter your name:")
+# print("welcome", name)
+# val= input("enter some value:")
+# print(type(val),val) 
+# name=input("enter name:")
+# age=int(input("enter age :"))
+# marks=float(input("enter marks :"))
+
+# print("welcome", name)
+# print("age =",age)
+# print("mark=",marks)
+
+# a=int(input("enter first :"))
+# b= int(input("enter second :"))
+
+# print(a>=b)
+# str1="This is a string.\twe are creating it in python."
+# str2='sonamkumari'
+# str3="""This is a string"""
+# print(str1)
+# print(str2)
+# print(str3)
+# concatenation
+# str1="sonam" 
+# print(len(str1))
+# str2="kumari"
+# print(len(str2))
+# final_string=str1+" "+str2
+# print(len(final_string))
+# print(final_string)
+str ="sonam kumari" 
+print(str [1:4])
